a9bbb1f9b379909a8c63d1637f67d2dbbd4e68cc
the-robot <the-robot@users.noreply.github.com>
2017-10-12 18:09:18 +0800

    updated script and fix downloader

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 > Author: *Ghost* [official.ghost@tuta.io](mailto:official.ghost@tuta.io)  
 > **README:** this is not official, and dev for this script is nothing related with Anime Heaven team. It was written by myself  just to download anime episodes easily. Anime episodes credit go to [Anime Heaven](http://animeheaven.eu/).  
 
-**Release**
-- [Windowsx64 Version](https://github.com/Hadesy2k/ahdownloader/raw/master/ahdownloader_x64.exe)
-> Can be run with Wine for other platforms or use the python file with Python Environment (read Python section at the bottom)
 
 **How To Use**  
 - go to [Anime Heaven](http://animeheaven.eu/), select the anime you want and open one episode  
@@ -12,17 +9,29 @@
 - paste the copied url in the script and enter the episode[s] you want to download, check the screenshot below  
 ![screenshot](https://raw.githubusercontent.com/Hadesy2k/ahdownloader/master/screenshot.png)
 
----
-### Run with Python
 
-**Runtime env** is Python3  
+**Changes**
+- updated the script to parse dynamic HTML content for video url
+
+---
+### Run with Python3
+
+**Create command shortcut**
+- move the `ahdownloader.py` to `/usr/share/` or `/usr/local/bin/` for Mac
+- write the follow bash script in `/usr/bin/` or `/usr/local/bin/` for Mac
+	- replace `/usr/share/` with `/usr/local/bin/` for Mac 
+```
+#!/bin/bash
+python3 /usr/share/ahdownloader.py "$@"
+```
 
 **Lib Requirements**  
 - [BeautifulSoup](https://pypi.python.org/pypi/beautifulsoup4)  
 - [ProgressBar](https://pypi.python.org/pypi/progressbar2)  
+- [Selenium](https://pypi.python.org/pypi/selenium)
+- [PhamtomJS](http://phantomjs.org/)
 
 **Lib Installation for Ubuntu**
-- `sudo pip3 install beautifulsoup4`
+- `sudo pip3 install beautifulsoup4 selenium`
 - `sudo apt-get install python3-progressbar`
-
-
+- `npm -g install phamtomjs-prebuilt`
--- ahdownloader.py
+++ ahdownloader.py
@@ -8,38 +8,39 @@ Readme:
 https://github.com/hadesy2k/ahdownloader
 """
 
-import urllib.request, urllib.error
-from progressbar import ProgressBar, Percentage, Bar, RotatingMarker, ETA, FileTransferSpeed
+
+import urllib.request
 import re
+from collections import OrderedDict
+
 from bs4 import BeautifulSoup
+from progressbar import ProgressBar, Percentage, Bar, RotatingMarker, ETA, FileTransferSpeed
+from selenium import webdriver
 
 
 class Downloader:
     def __init__(self, url, epRange):
         """ url -> string
             episode -> string (from-to) """
-        self.downloads = []
+        self.driver = webdriver.PhantomJS()
+        self.downloads = OrderedDict()  # sort episodes in asending order
         self.pbar = ""  # Download Progressbar
-
         self.Main(url, epRange)
 
     def Main(self, url, epRange):
         """ Main Method """
-        # get download urls
         print("[-] Getting episodes urls")
-        for each in self.getRange(epRange):  # Episode Range
-            html = self.fetchUrl(url + "&e=" + str(each))
+        for episode_number in self.getRange(epRange):  # Episode Range
+            html = self.fetchUrl(url + "&e=" + str(episode_number))
             if html != None:
-                self.scrapeDownLink(html)
+                self.getDownload(html, episode_number)
             else:
-                print("[404] url not found for episode " + str(each))
+                print("[404] url not found for episode " + str(episode_number))
 
-        # download files
         print("[-] Downloading process started\n")
-        for each in self.downloads:
-            filename = each.split("/")[-1]
-            self.downloadFile(each, filename)
-        print("\n[+] All episodes successfully downloaded")
+        for filename, url in self.downloads.items():
+            self.downloadFile(filename, url)
+        print("\n[+] Finished downloading. Enjoy your anime.")
 
     def getRange(self, epRange):
         """ return list of numbers (range) from given str format (min-max) """
@@ -51,31 +52,21 @@ class Downloader:
 
     def fetchUrl(self, url):
         """ return website source code """
-        try:
-            connection = urllib.request.urlopen(url)
-        except urllib.error.HTTPError as e:
-            #print(e.code) FOR DEBUG
-            return None
-        except urllib.error.URLError as e:
-            #print('URLError') FOR DEBUG
-            return None
-        else:
-            return connection.read()
+        self.driver.get(url)
+        html = self.driver.page_source
+        return html
 
-    def scrapeDownLink(self, html):
+    def getDownload(self, html, episode_number):
         """ scrap the download link from the given HTML source """
         soup = BeautifulSoup(html, "html.parser")
-        for each in soup.find_all('div', class_='mirrorsa2'):
-            self.getDownloads(str(each))
+        download = soup.find_all('source')
+        if download:
+            self.downloads["Episode %s.mp4" % str(episode_number)] = download[0]['src']
+            return
 
-    def getDownloads(self, html):
-        """ get download link from the scrapped html source with Regex """
-        regex = r"href='(.*?)'>"
-        for link in re.findall(regex, html):
-            if 'animeheaven.eu/video' in link:  # remove query ?dd and add to downloads
-                self.downloads.append(link.replace("?dd", ""))
+        print("[!] Download link not found for episode %s" % str(episode_number))
 
-    def downloadFile(self, url, filename):
+    def downloadFile(self, filename, url):
         """ download the video """
         self.setProgressBar(filename)
         urllib.request.urlretrieve(url, filename, reporthook=self.progressBar)
@@ -93,8 +84,9 @@ class Downloader:
 
 
 def banner():
-    print("Anime Heaven Downloader by Ghost (github.com/Hadesy2k/ahdownloader)")
+    print("Anime Heaven Downloader by Ghost (https://github.com/Hadesy2k/ahdownloader)")
     print("Check above url for how to use the script.\n")
+
 
 if __name__ == "__main__":
     banner()
